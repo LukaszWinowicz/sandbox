b1e7cfabeab0940b41a735498be7fcb319c9001d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-11 16:47:28 +0200

    Zastosowanie wzorca adapter jako mapper

---
+++ src/02_StructuralsPatterns/AdapterPattern/UserMapper.cs
@@ -0,0 +1,15 @@
+﻿namespace AdapterPattern;
+
+// Adapter
+public class UserMapper
+{
+    public UserDto Map(User user)
+    {
+        return new UserDto { Id = user.Id, FirstName = user.FirstName, LastName = user.LastName, Email = user.Email };
+    }
+
+    public User Map(UserDto dto)
+    {
+        return new User { Id = dto.Id, FirstName = dto.FirstName, LastName = dto.LastName, Email = dto.Email };
+    }   
+}
