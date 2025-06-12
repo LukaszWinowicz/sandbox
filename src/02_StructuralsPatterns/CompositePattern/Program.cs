2b0f5e3ef0bab89fbed059443e39b522c4474a96
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-12 14:18:12 +0200

    Zastosowanie composite do drzewa decyzyjnego

--- src/02_StructuralsPatterns/CompositePattern/Program.cs
+++ src/02_StructuralsPatterns/CompositePattern/Program.cs
@@ -3,31 +3,76 @@ namespace CompositePattern;
 class Program
 {
     static void Main(string[] args)
-    {       
-        Console.Write("Are you developer?");       
+    {
+        var notForYou = new Decision("The Course is not for you.", false);
+        var welcome = new Decision("Welcome on Design Pattern in C# Course!", true);
 
-        if (Response) {
+        var dpQuestion = new Question("Do you know Design Pattern?", notForYou, welcome);
 
-            Console.Write("Do you know C#?");
+        var csharpQuestion = new Question("Do you know C#?", dpQuestion, notForYou);
+        var developerQuestion = new Question("Are you developer?", csharpQuestion, notForYou);
 
-            if (Response)
-            {
-                Console.WriteLine("Welcome on Design Pattern in C# Course!");
-            }
-            else
-            {
-                Console.WriteLine("The Course is not for you.");
-            }
 
+        developerQuestion.Message();
+
+    }
+}
+
+// Abstract Component
+public interface IQuestion
+{
+    void Message();
+}
+
+
+// Concrete Component - Branch
+public class Question : IQuestion
+{
+    private readonly string prompt;
+    private readonly IQuestion positive;
+    private readonly IQuestion negative;
+
+    public Question(string prompt, IQuestion positive, IQuestion negative)
+    {
+        this.prompt = prompt;
+        this.positive = positive;
+        this.negative = negative;
+    }
+
+    public void Message()
+    {
+        Console.Write(prompt);
+
+        if (Response)
+        {
+            positive.Message();
         }
         else
         {
-            Console.WriteLine("Have a nice day.");
+            negative.Message();
         }
-
-
     }
 
     public static bool Response => Console.ReadKey().Key == ConsoleKey.Y;
-
 }
+
+// Concrete Component - Leaf
+public class Decision : IQuestion
+{
+    private readonly string message;
+    private readonly bool positive;
+
+    public Decision(string message, bool positive)
+    {
+        this.message = message;
+        this.positive = positive;
+    }
+
+    public void Message()
+    {
+        Console.BackgroundColor = positive ? ConsoleColor.Green : ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+}
+
