7405fe028c4f8c81303f9260c936453e6462ac1b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-13 14:28:39 +0200

    Refactor: Zastosowanie wzorca komendy

--- src/03_BehavioralsPatterns/CommandPattern/Program.cs
+++ src/03_BehavioralsPatterns/CommandPattern/Program.cs
@@ -1,25 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace CommandPattern
+namespace CommandPattern;
+
+
+public class CommandFactory
 {
-    class Program
+    private readonly IDictionary<string, ICommand> commmands = new Dictionary<string, ICommand>();
+
+    private Message message;
+
+    public CommandFactory()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Command Pattern!");
-
-            Message message = new Message("555000123", "555888000", "Hello World!");
-
-            if (message.CanPrint())
-            {
-                message.Print();
-            }
-
-            if (message.CanSend())
-            {
-                message.Send();
-            }    
-        }
+        commmands.Add("print", new PrintCommand(message));
+        commmands.Add("send", new PrintCommand(message));
+        commmands.Add("save", new SaveCommand(message, "plik"));
     }
 
+    public ICommand Create(string arg)
+    {
+        return commmands[arg];
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Command Pattern!");
+
+        CommandFactory factory = new CommandFactory();
+
+        ICommand command = factory.Create(args[0]);
+
+
+        QueueCommandTest();
+
+    }
+
+    private static void QueueCommandTest()
+    {
+        Message message = new Message("555000123", "555888000", "Hello World!");
+
+        Queue<ICommand> queue = new Queue<ICommand>();
+
+        queue.Enqueue(new PrintCommand(message));
+        queue.Enqueue(new SendCommand(message));
+        queue.Enqueue(new SendCommand(message));
+        queue.Enqueue(new SaveCommand(message, "message.txt"));
+
+        while (queue.Count > 0)
+        {
+            ICommand command = queue.Dequeue();
+
+            command.Execute();
+        }
+    }
 }
