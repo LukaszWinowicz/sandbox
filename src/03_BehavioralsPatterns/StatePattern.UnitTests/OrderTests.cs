5361e2cce9e1189094ae6b09f2db648c10fa5d61
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-13 15:53:07 +0200

    Refactor: Zastosowanie maszyny stanów skończonych

--- src/03_BehavioralsPatterns/StatePattern.UnitTests/OrderTests.cs
+++ src/03_BehavioralsPatterns/StatePattern.UnitTests/OrderTests.cs
@@ -3,126 +3,140 @@ using System;
 using FluentAssertions;
 using Xunit;
 
-namespace StatePattern.UnitTests
+namespace StatePattern.UnitTests;
+
+
+public class OrderTests
 {
-
-    public class OrderTests
+    [Fact]
+    public void Order_WhenCreated_ShouldBePending()
     {
-        [Fact]
-        public void Order_WhenCreated_ShouldBePending()
-        {
-            // Arrange
-            Order order = new Order();
+        // Arrange
+        Order order = new Order();
 
-            // Act
-            var result = order.Status;
+        // Act
+        var result = order.Status;
 
-            // Assert
-            result.Should().Be(OrderStatus.Placement);
+        // Assert
+        result.Should().Be(OrderStatus.Placement);
 
-        }
+    }
 
-        [Fact]
-        public void Confirm_NotPaid_ShouldBePlacement()
-        {
-            // Arrange
-            Order order = new Order();            
+    [Fact]
+    public void Confirm_NotPaid_ShouldBePlacement()
+    {
+        // Arrange
+        Order order = new Order();
+        order.Confirm();
 
-            // Act
-            order.Confirm();
+        // Act
+        order.Confirm();
 
-            // Assert
-            order.Status.Should().Be(OrderStatus.Placement);
-        }
+        // Assert
+        order.Status.Should().Be(OrderStatus.Canceled);
+    }
 
-        [Fact]
-        public void Confirm_Paid_ShouldBePicking()
-        {
-            // Arrange
-            Order order = new Order();
-            order.Paid();
+    [Fact]
+    public void Confirm_Paid_ShouldBePicking()
+    {
+        // Arrange
+        Order order = new Order();
+        order.Paid();
 
-            // Act
-            order.Confirm();
+        // Act
+        order.Confirm();
 
-            // Assert
-            order.Status.Should().Be(OrderStatus.Picking);
-        }
+        // Assert
+        order.Status.Should().Be(OrderStatus.Picking);
+    }
 
-        [Theory]
-        [InlineData(OrderStatus.Picking, OrderStatus.Shipping)]        
-        [InlineData(OrderStatus.Shipping, OrderStatus.Delivered)]
-        [InlineData(OrderStatus.Delivered, OrderStatus.Completed)]
-        public void Confirm_WhenSourceStatus_ShouldBeStatus(OrderStatus sourceStatus, OrderStatus expected)
-        {
-            // Arrange        
-            Order order = new Order(sourceStatus);
+    [Theory]
+    [InlineData(OrderStatus.Picking, OrderStatus.Shipping)]        
+    [InlineData(OrderStatus.Shipping, OrderStatus.Delivered)]
+    [InlineData(OrderStatus.Delivered, OrderStatus.Completed)]
+    public void Confirm_WhenSourceStatus_ShouldBeStatus(OrderStatus sourceStatus, OrderStatus expected)
+    {
+        // Arrange        
+        Order order = new Order(sourceStatus);
 
-            // Act
-            order.Confirm();
+        // Act
+        order.Confirm();
 
-            // Assert
-            order.Status.Should().Be(expected);
-        }
+        // Assert
+        order.Status.Should().Be(expected);
+    }
 
-        [Theory]
-        [InlineData(OrderStatus.Placement, OrderStatus.Canceled)]
-        [InlineData(OrderStatus.Delivered, OrderStatus.Canceled)]
-        public void Cancel_WhenSourceStatus_ShouldBeStatus(OrderStatus sourceStatus, OrderStatus expected)
-        {
-            // Arrange        
-            Order order = new Order(sourceStatus);
+    [Theory]
+    [InlineData(OrderStatus.Placement, OrderStatus.Canceled)]
+    [InlineData(OrderStatus.Delivered, OrderStatus.Canceled)]
+    public void Cancel_WhenSourceStatus_ShouldBeStatus(OrderStatus sourceStatus, OrderStatus expected)
+    {
+        // Arrange        
+        Order order = new Order(sourceStatus);
 
-            // Act
-            order.Cancel();
+        // Act
+        order.Cancel();
 
-            // Assert
-            order.Status.Should().Be(expected);
-        }
+        // Assert
+        order.Status.Should().Be(expected);
+    }
 
-        [Fact]
-        public void Cancel_WhenSentStatus_ShouldThrowInvalidOperationException()
-        {
-            // Arrange        
-            Order order = new Order(OrderStatus.Shipping);
+    [Fact]
+    public void Cancel_WhenSentStatus_ShouldThrowInvalidOperationException()
+    {
+        // Arrange        
+        Order order = new Order(OrderStatus.Shipping);
 
-            // Act
-            var act = () => order.Cancel();
+        // Act
+        var act = () => order.Cancel();
 
-            // Assert
-            act.Should().Throw<InvalidOperationException>();
-        }
+        // Assert
+        act.Should().Throw<InvalidOperationException>();
+    }
 
-        [Fact]
-        public void Cancel_WhenCompletedStatus_ShouldThrowInvalidOperationException()
-        {
-            // Arrange        
-            Order order = new Order(OrderStatus.Completed);
+    [Fact]
+    public void Cancel_WhenCompletedStatus_ShouldThrowInvalidOperationException()
+    {
+        // Arrange        
+        Order order = new Order(OrderStatus.Completed);
 
-            // Act
-            var act = () => order.Cancel();
+        // Act
+        var act = () => order.Cancel();
 
-            // Assert
-            act.Should().Throw<InvalidOperationException>();
-        }
+        // Assert
+        act.Should().Throw<InvalidOperationException>();
+    }
 
 
 
-        [Theory]
-        [InlineData(OrderStatus.Placement, true)]
-        [InlineData(OrderStatus.Shipping, true)]
-        [InlineData(OrderStatus.Delivered, true)]
-        [InlineData(OrderStatus.Completed, false)]
-        public void CanConfirm_WhenStatus_ShouldBe(OrderStatus sourceStatus, bool expected)
-        {
-            // Arrange        
-            Order order = new Order(sourceStatus);
+    [Theory]
+    [InlineData(OrderStatus.Placement, true)]
+    [InlineData(OrderStatus.Shipping, true)]
+    [InlineData(OrderStatus.Delivered, true)]
+    [InlineData(OrderStatus.Completed, false)]
+    public void CanConfirm_WhenStatus_ShouldBe(OrderStatus sourceStatus, bool expected)
+    {
+        // Arrange        
+        Order order = new Order(sourceStatus);
 
-            // Act
-            var result = order.CanConfirm;
+        // Act
+        var result = order.CanConfirm;
 
-            // Assert
-            result.Should().Be(expected);
-        }
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Graph_WhenCalled_ShouldReturnMermaidGraph()
+    {
+        // Arrange
+        Order order = new Order();
+
+        // Act
+        var result = order.Graph;
+
+        // Assert
+        result.Should().NotBeNullOrEmpty();
+
     }
 }
--- src/03_BehavioralsPatterns/StatePattern/Models/Order.cs
+++ src/03_BehavioralsPatterns/StatePattern/Models/Order.cs
@@ -1,95 +1,83 @@
-﻿using System;
+﻿using Stateless;
+using System;
 using System.Diagnostics.Tracing;
 
-namespace StatePattern
+
+
+namespace StatePattern;
+
+public class Order
 {
-    public class Order
+    // dotnet add package Stateless
+    private StateMachine<OrderStatus, OrderTrigger> machine;
+
+    public Order(OrderStatus initialState = OrderStatus.Placement)
     {
+        Id = Guid.NewGuid();
+        OrderDate = DateTime.Now;
 
-        public Order(OrderStatus initialState = OrderStatus.Placement)
-        {            
-            Id = Guid.NewGuid();
-            OrderDate = DateTime.Now;
-            Status = initialState;
-        }
+        machine = new StateMachine<OrderStatus, OrderTrigger>(initialState);
 
-        public Guid Id { get; set; }
-        public DateTime OrderDate { get; set; }
-        public OrderStatus Status { get; private set; }
-        public bool IsPaid { get; private set; }       
+        machine.Configure(OrderStatus.Placement)
+            .Permit(OrderTrigger.Confirm, OrderStatus.Picking)
+            .Permit(OrderTrigger.Cancel, OrderStatus.Canceled);
 
-        public void Paid()
-        {
-            IsPaid = true;
-        }
+        machine.Configure(OrderStatus.Picking)
+            .PermitIf(OrderTrigger.Confirm, OrderStatus.Shipping, () => IsPaid, "Paid") // jesli zapłacono
+            .PermitIf(OrderTrigger.Confirm, OrderStatus.Canceled, () => !IsPaid, "NotPaid"); // jeśli nie zapłacono
 
-        public void Confirm()
-        {
-            if (Status == OrderStatus.Placement)
-            {
-                if (IsPaid)
-                {
-                    Status = OrderStatus.Picking;
-                }                
-            }
-            else if (Status == OrderStatus.Picking)
-            {
-                Status = OrderStatus.Shipping;
-            }
-            else if (Status == OrderStatus.Shipping)
-            {
-                Status = OrderStatus.Delivered;
+        machine.Configure(OrderStatus.Shipping)
+            .Permit(OrderTrigger.Confirm, OrderStatus.Delivered);
 
-                Console.WriteLine("You order was delivered.");
-            }
-            else if (Status == OrderStatus.Delivered)
-            {
-                Status = OrderStatus.Completed;
-            }
-            else
-            {
-                throw new InvalidOperationException();
-            }
-
-        }
-
-        public void Cancel()
-        {
-            if (Status == OrderStatus.Placement)
-            {
-                Status = OrderStatus.Canceled;
-            }
-            else if (Status == OrderStatus.Delivered)
-            {
-                Status = OrderStatus.Canceled;
-            }
-            else
-            {
-                throw new InvalidOperationException();
-            }
-        }
-
-        public override string ToString() => $"Order {Id} created on {OrderDate}{Environment.NewLine}";
-
-        public virtual bool CanConfirm => Status == OrderStatus.Placement || Status == OrderStatus.Shipping || Status == OrderStatus.Delivered;
-        public virtual bool CanCancel => Status == OrderStatus.Placement || Status == OrderStatus.Delivered;
-    }
-
-    public enum OrderStatus
-    {
-        // The customer places an order on the company's website
-        Placement,
-        // The items from the order are picked from the inventory
-        Picking,
-        // The package is handed over to the shipping carrier or courier for delivery to the customer.      
-        Shipping,
-        // The package has been successfully delivered to the customer's specified address.
-        Delivered,
-        // The order has been successfully delivered, and the transaction is considered complete.
-        Completed,
-        // The customer canceled order
-        Canceled
+        machine.Configure(OrderStatus.Delivered)
+            .OnEntry(() => Console.WriteLine("You order was delivered."))
+            .Permit(OrderTrigger.Confirm, OrderStatus.Completed)
+            .Permit(OrderTrigger.Cancel, OrderStatus.Canceled);
 
     }
 
+
+    public string Graph => Stateless.Graph.MermaidGraph.Format(machine.GetInfo());
+
+    public Guid Id { get; set; }
+    public DateTime OrderDate { get; set; }
+    public OrderStatus Status => machine.State;
+    public bool IsPaid { get; private set; }
+
+    public void Paid()
+    {
+        IsPaid = true;
+    }
+
+    public void Confirm() => machine.Fire(OrderTrigger.Confirm);
+
+    public void Cancel() => machine.Fire(OrderTrigger.Cancel);
+
+    public override string ToString() => $"Order {Id} created on {OrderDate}{Environment.NewLine}";
+
+    public virtual bool CanConfirm => Status == OrderStatus.Placement || Status == OrderStatus.Shipping || Status == OrderStatus.Delivered;
+    public virtual bool CanCancel => Status == OrderStatus.Placement || Status == OrderStatus.Delivered;
+}
+
+public enum OrderStatus
+{
+    // The customer places an order on the company's website
+    Placement,
+    // The items from the order are picked from the inventory
+    Picking,
+    // The package is handed over to the shipping carrier or courier for delivery to the customer.      
+    Shipping,
+    // The package has been successfully delivered to the customer's specified address.
+    Delivered,
+    // The order has been successfully delivered, and the transaction is considered complete.
+    Completed,
+    // The customer canceled order
+    Canceled
+
+}
+
+public enum OrderTrigger
+{
+    Confirm,
+    Cancel,
 }
